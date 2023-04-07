c05d5b836731fbe30937c4f485f7e451ccd1e65e
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-07 13:57:34 -0700

    Adding basic Navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,13 @@ import { BsCart4 } from 'react-icons/bs'
 function Navbar() {
     return (
         <div className="navbar">
-            <div className="title">Studio306.</div>
+            <div className="title">Studio306</div>
 
-            <RxHamburgerMenu />
-            <BsCart4 />
+            <div className="menu">
+                <RxHamburgerMenu />
+                <BsCart4 />
+            </div>
+
         </div>
     )
 }
