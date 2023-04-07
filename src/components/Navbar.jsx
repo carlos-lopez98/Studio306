dad06721417df4501da8209ff88d038c2decdfa0
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-07 13:44:07 -0700

    Importing some dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
-
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { BsCart4 } from 'react-icons/bs'
 
 
 function Navbar() {
     return (
-        <div>
+        <div className="navbar">
+            <div className="title">Studio306.</div>
 
+            <RxHamburgerMenu />
+            <BsCart4 />
         </div>
     )
 }
